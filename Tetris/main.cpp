f96d40aba8254890a627bbf9b156c85738085a45
Gaetz <gaetan.blaisecazalet@gmail.com>
2018-02-22 01:27:52 +0100

    Limit FPS to 60

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -3,13 +3,28 @@
 Game *game = nullptr;
 
 int main(int argc, char **argv) {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 	game->init("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	while (game->running()) {
+		frameStart = SDL_GetTicks();
+
+		// Game loop
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		// Delay frame if game runs too fast
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameTime < frameDelay) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
